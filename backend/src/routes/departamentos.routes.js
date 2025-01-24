0588e6a5f9063306514cb3b3f219c68b59733517
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-23 20:56:18 -0500

    [actualizar] rutas

---
+++ backend/src/routes/departamentos.routes.js
@@ -0,0 +1,20 @@
+// // IMPORTAR CONTROLADORES Y DEPENDENCIAS
+import { postDepartamento, getDepartamento, putDepartamentoById, deleteDepartamentoById } from '../controllers/departamentos.controller.js';
+import express from 'express';
+
+// CONFIGURAR ROUTER
+export const departamentosRouter = express.Router;
+
+// RUTAS PARA LAS PETICIONES
+
+// POST
+departamentosRouter.post('/crear', postDepartamento);
+
+// GET
+departamentosRouter.get('/obtener', getDepartamento);
+
+// PUT
+departamentosRouter.put('/actualizar', putDepartamentoById);
+
+// DELETE
+departamentosRouter.delete('/eliminar', deleteDepartamentoById);
---
+++ backend/src/routes/empleados.routes.js
@@ -0,0 +1,20 @@
+// IMPORTAR CONTROLADORES Y DEPENDENCIAS
+import { postEmpleado, getEmpleado, putEmpleadoById, deleteEmpleadoById } from "../controllers/empleados.controller.js";
+import express from 'express';
+
+// CONFIGURAR ROUTER
+export const empleadosRouter = express.Router;
+
+// RUTAS PARA LAS PETICIONES
+
+// POST
+empleadosRouter.post('/crear', postEmpleado);
+
+// GET
+empleadosRouter.get('/obtener', getEmpleado);
+
+// PUT
+empleadosRouter.put('/actualizar', putEmpleadoById);
+
+// DELETE
+empleadosRouter.delete('/eliminar', deleteEmpleadoById);
