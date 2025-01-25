6df3f0bb91f53e97466c8597ea449fa7ae9d675e
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-25 18:02:01 -0500

    [actualizar] guards e interceptors

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+//Dependencia para conectar el backend con el frontend
+import { provideHttpClient } from '@angular/common/http';
+
+// Gestion de las rutas
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+
+    // Proveedor para hacer peticiones al backend y conectarse al backend
+    provideHttpClient(),
+    
+  ]
 };
