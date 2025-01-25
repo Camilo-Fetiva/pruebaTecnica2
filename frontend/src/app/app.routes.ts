b8824ff6a637cf3224aa396a1e20dade4a31a8fc
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-25 15:54:10 -0500

    [actualizar] visuales creadas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// IMPORTAR LAS RUTAS
+import { HomeComponent } from './pages/home/home.component';
+import { FormDepartamentosComponent } from './pages/form-departamentos/form-departamentos.component';
+import { FormEmpleadosComponent } from './pages/form-empleados/form-empleados.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent, title:'Home'},
+    {path: 'FormularioDepartamentos', component: FormDepartamentosComponent, title:'FormularioDepartamentos'},
+    {path: 'FormularioEmpleados', component: FormEmpleadosComponent, title:'FormularioEmpleados'},
+];
